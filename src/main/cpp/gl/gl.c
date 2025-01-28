3be19db7667334f16a35843f4d15cc60df437a9f
BZLZHH <admin@bzlzhh.top>
2025-01-29 01:19:11 +0800

    [Fix|Chore] (gl.c|...): Fix the sequence error of GL_RENDERER and GL_VERSION. Remove useless file.
    Signed-off-by: BZLZHH <admin@bzlzhh.top>

--- src/main/cpp/gl/gl.c
+++ src/main/cpp/gl/gl.c
@@ -16,20 +16,16 @@ GLAPI GLAPIENTRY GLenum glGetError() {
 	return gles_glGetError();
 }
 
-static char renderer_str[512];
 GLAPI GLAPIENTRY const GLubyte * glGetString( GLenum name ) {
 	LOG();
     LOAD_GLES(glGetString, const GLubyte *, GLenum);
 	switch (name) {
 	case GL_VENDOR:
 		return (const GLubyte *)"Swung0x48, BZLZHH";
-	case GL_RENDERER:
-		return (const GLubyte *)"2.1 MobileGlues";
-	case GL_VERSION: {
-        GLubyte *orig_str = gles_glGetString(GL_RENDERER);
-        snprintf(renderer_str, 512 - 1, "2.1.0 %s", orig_str);
-        return (const GLubyte *) renderer_str;
-    }
+	case GL_VERSION:
+		return (const GLubyte *)"3.0 MobileGlues";
+	case GL_RENDERER: 
+        return gles_glGetString(GL_RENDERER);
 	}
 	return (const GLubyte *)"NotSupported_GLenum";
 }
