193bce24140ee55cc0be4b94e6d5224ece6aba43
由风 <youfeng11@outlook.com>
2025-08-11 16:30:41 +0800

    [Chore]: Migrate build script to Kotlin DSL and bump compileSdk
    This commit migrates the `build.gradle` file to `build.gradle.kts`, utilizing the Kotlin DSL for Gradle.
    It also updates the `compileSdk` version from 35 to 36.
    The `debuggable` property was removed from the `fordebug` build type as it's true by default for debug build types.

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id("com.android.library")
+}
+
+android {
+    namespace = "com.swung0x48.mobileglues"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 21
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+        ndkVersion = "27.1.12297006"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        create("proguard") {
+            isMinifyEnabled = true
+            initWith(getByName("debug"))
+        }
+        create("fordebug") {
+        }
+    }
+    externalNativeBuild {
+        cmake {
+            path = file("src/main/cpp/CMakeLists.txt")
+            version = "3.22.1"
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
